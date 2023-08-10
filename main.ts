139316cdc0f5793d1421dab3f678261f8f9f5e85
Matheus Reichemback Stang <matheusstang15@gmail.com>
2023-08-10 17:04:03 -0300

    1 commit

---
+++ main.ts
@@ -0,0 +1,21 @@
+// ########################~Primeira Tarefa~########################
+// Variáveis que entregam os valores para Arrow Function
+let altura: number = 2
+let base: number = 7
+
+// Arrow Function que retorna a área
+const Area = (altura: number, base: number) => altura * base
+
+// Mostrando o resultado da conta
+console.log(Area(altura, base))
+
+// ########################~Segunda Tarefa~########################
+// Variáveis que entregam os nomes para Arrow Function
+let nome: string = 'Matheus'
+let sobrenome: string = 'Reichemback Stang'
+
+// Arrow Function que retorna a cumprimento
+const dizOi = (nomeDaPessoa: string, sobrenomeDaPessoa: string) => `Olá ${nomeDaPessoa} ${sobrenomeDaPessoa}`
+
+// Mostrando o resultado da Arrow Function
+console.log(dizOi(nome, sobrenome))
